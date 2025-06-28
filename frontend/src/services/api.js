58c7fdd9523b16a1e04802aebdbeeeabd0449e55
Agu <agustinpbrk04@gmail.com>
2025-06-28 15:42:27 -0300

    Front Conectado

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api'
+  baseURL: 'https://gestor-de-gastos-backend.onrender.com/api',
 });
 
 export const registerUser = (data) => api.post('/users/register', data);
